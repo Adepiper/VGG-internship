7f648bac1f285be0301f9d1b61d691d572d66672
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-02-09 04:53:44 +0100

    'deploy'

--- vgginternship/config.py
+++ vgginternship/config.py
@@ -1,4 +1,4 @@
 class Config: 
     SECRET_KEY = 'e4b1815c5823da1786c3e1064793fec2'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///VGGinternship.db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://zgmtnblwwuddsg:6eb8ab5364742a428b3df2996d9a5fe33555762ac3f415597966c6a5fa20bced@ec2-52-203-160-194.compute-1.amazonaws.com:5432/da85u9e2mvite9'
