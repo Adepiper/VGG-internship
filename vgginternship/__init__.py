ce3b5938b6560d0f95135d137bc4d3f1e001454a
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-02-09 05:31:26 +0100

    Update __init__.py

--- vgginternship/__init__.py
+++ vgginternship/__init__.py
@@ -30,11 +30,11 @@ def create_app():
     ENV = 'prod'
 
     if ENV == 'dev' : 
-    app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Adeola2009.@localhost/VggInternship'
+        app.debug = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Adeola2009.@localhost/VggInternship'
     else :
-    app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zgmtnblwwuddsg:6eb8ab5364742a428b3df2996d9a5fe33555762ac3f415597966c6a5fa20bced@ec2-52-203-160-194.compute-1.amazonaws.com:5432/da85u9e2mvite9'
+        app.debug = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zgmtnblwwuddsg:6eb8ab5364742a428b3df2996d9a5fe33555762ac3f415597966c6a5fa20bced@ec2-52-203-160-194.compute-1.amazonaws.com:5432/da85u9e2mvite9'
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
