d518b33e830eb0538d59d1a4bdfb936ef035d4cc
Adepiper <52428747+Adepiper@users.noreply.github.com>
2020-02-09 05:26:07 +0100

    push

--- vgginternship/__init__.py
+++ vgginternship/__init__.py
@@ -12,8 +12,10 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = 'e4b1815c5823da1786c3e1064793fec2'
 
     #ENV = 'prod'
 
@@ -25,11 +27,17 @@ def create_app(config_class=Config):
         #app.debug = False
        
         #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vvtotfjyntpcaf:94b0960541c95c579ee2b6167bce87e5390397cd051a49888aa60a6d55f4b77f@ec2-3-234-109-123.compute-1.amazonaws.com:5432/d48mi4tt17c84'
-    app.config.from_object(Config)
+    ENV = 'prod'
+
+    if ENV == 'dev' : 
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Adeola2009.@localhost/VggInternship'
+    else :
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zgmtnblwwuddsg:6eb8ab5364742a428b3df2996d9a5fe33555762ac3f415597966c6a5fa20bced@ec2-52-203-160-194.compute-1.amazonaws.com:5432/da85u9e2mvite9'
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
+
 
     from vgginternship.users.routes import users
     from vgginternship.projects.routes import projects
